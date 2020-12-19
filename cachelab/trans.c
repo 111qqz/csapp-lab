c081ab418df53da5cc4e66b090bbefd3409bc688
111qqz <hust.111qqz@gmail.com>
2020-12-19 19:36:29 +0800

    using loop unroll trick to get 287 miss in (N=32,M=32) setting

--- cachelab/trans.c
+++ cachelab/trans.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * trans.c - Matrix transpose B = A^T
  *
  * Each transpose function must have a prototype of the form:
@@ -6,44 +6,118 @@
  *
  * A transpose function is evaluated by counting the number of misses
  * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
+ */
 #include <stdio.h>
+
 #include "cachelab.h"
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
-/* 
+/*
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
  *     the description string "Transpose submission", as the driver
  *     searches for that string to identify the transpose function to
- *     be graded. 
+ *     be graded.
  */
 char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N])
-{
+void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
+  int i, j, tmp;
+
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < M; j++) {
+      tmp = A[i][j];
+      B[j][i] = tmp;
+    }
+  }
 }
 
-/* 
+/*
  * You can define additional transpose functions below. We've defined
- * a simple one below to help you get started. 
- */ 
+ * a simple one below to help you get started.
+ */
 
-/* 
+/*
  * trans - A simple baseline transpose function, not optimized for the cache.
  */
 char trans_desc[] = "Simple row-wise scan transpose";
-void trans(int M, int N, int A[N][M], int B[M][N])
-{
-    int i, j, tmp;
+void trans(int M, int N, int A[N][M], int B[M][N]) {
+  int i, j, tmp;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
-            tmp = A[i][j];
-            B[j][i] = tmp;
-        }
-    }    
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < M; j++) {
+      tmp = A[i][j];
+      B[j][i] = tmp;
+    }
+  }
+}
 
+char trans_desc_loop_unroll[] = "loop unroll";
+void trans_loop_unroll(int M, int N, int A[N][M], int B[M][N]) {
+  int i, j;
+  // 循环展开。。
+  int tmp, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
+  for (i = 0; i < N; i += 8) {
+    for (j = 0; j < M; j++) {
+      tmp = A[i][j];
+      tmp1 = A[i + 1][j];
+      tmp2 = A[i + 2][j];
+      tmp3 = A[i + 3][j];
+      tmp4 = A[i + 4][j];
+      tmp5 = A[i + 5][j];
+      tmp6 = A[i + 6][j];
+      tmp7 = A[i + 7][j];
+      B[j][i] = tmp;
+      B[j][i + 1] = tmp1;
+      B[j][i + 2] = tmp2;
+      B[j][i + 3] = tmp3;
+      B[j][i + 4] = tmp4;
+      B[j][i + 5] = tmp5;
+      B[j][i + 6] = tmp6;
+      B[j][i + 7] = tmp7;
+    }
+  }
+}
+
+char trans_desc_blocking[] = "Simple row-wise scan transpose";
+void trans_blocking(int M, int N, int A[N][M], int B[M][N]) {
+  int tmp, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
+  //   for (i = 0; i < N; i++) {
+  //     for (j = 0; j < M; j++) {
+  //       tmp = A[i][j];
+  //       B[j][i] = tmp;
+  //     }
+  //   }
+  for (int block_i = 0; block_i < N / 8; block_i++) {
+    for (int block_j = 0; block_j < M / 8; block_j++) {
+      for (int i = 0; i < 8; i++) {
+        tmp = A[i + 8 * block_i][8 * block_j];
+        tmp1 = A[i + 8 * block_i][8 * block_j + 1];
+        tmp2 = A[i + 8 * block_i][8 * block_j + 2];
+        tmp3 = A[i + 8 * block_i][8 * block_j + 3];
+        tmp4 = A[i + 8 * block_i][8 * block_j + 4];
+        tmp5 = A[i + 8 * block_i][8 * block_j + 5];
+        tmp6 = A[i + 8 * block_i][8 * block_j + 6];
+        tmp7 = A[i + 8 * block_i][8 * block_j + 7];
+
+        B[8 * block_j][i + 8 * block_i] = tmp;
+        B[8 * block_j + 1][i + 8 * block_i] = tmp1;
+        B[8 * block_j + 2][i + 8 * block_i] = tmp2;
+        B[8 * block_j + 3][i + 8 * block_i] = tmp3;
+        B[8 * block_j + 4][i + 8 * block_i] = tmp4;
+        B[8 * block_j + 5][i + 8 * block_i] = tmp5;
+        B[8 * block_j + 6][i + 8 * block_i] = tmp6;
+        B[8 * block_j + 7][i + 8 * block_i] = tmp7;
+      }
+      // for ( int i = 0 ; i < 8 ;  i++){
+      //     for (int j = 0 ; j < 8 ; j++){
+      //         tmp = A[i+8*block_i][j+8*block_j];
+
+      //         B[j+8*block_j][i+8*block_i] = tmp;
+      //     }
+      // }
+    }
+  }
 }
 
 /*
@@ -53,32 +127,31 @@ void trans(int M, int N, int A[N][M], int B[M][N])
  *     performance. This is a handy way to experiment with different
  *     transpose strategies.
  */
-void registerFunctions()
-{
-    /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+void registerFunctions() {
+  /* Register your solution function */
+  registerTransFunction(transpose_submit, transpose_submit_desc);
+  /* Register any additional transpose functions */
+  registerTransFunction(trans, trans_desc);
 
-    /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
+  registerTransFunction(trans_loop_unroll, trans_desc_loop_unroll);
 
+//   registerTransFunction(trans_blocking, trans_desc_blocking);
 }
 
-/* 
+/*
  * is_transpose - This helper function checks if B is the transpose of
  *     A. You can check the correctness of your transpose by calling
  *     it before returning from the transpose function.
  */
-int is_transpose(int M, int N, int A[N][M], int B[M][N])
-{
-    int i, j;
+int is_transpose(int M, int N, int A[N][M], int B[M][N]) {
+  int i, j;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; ++j) {
-            if (A[i][j] != B[j][i]) {
-                return 0;
-            }
-        }
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < M; ++j) {
+      if (A[i][j] != B[j][i]) {
+        return 0;
+      }
     }
-    return 1;
+  }
+  return 1;
 }
-
