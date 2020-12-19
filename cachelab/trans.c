1886cf66dfe9fdc29cdb89be0c6ec3939626e8f5
111qqz <hust.111qqz@gmail.com>
2020-12-19 21:01:03 +0800

    using blocking && loop unroll trick to get 1997 miss in (N=67,M=61) setting

--- cachelab/trans.c
+++ cachelab/trans.c
@@ -12,7 +12,7 @@
 #include "cachelab.h"
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
-
+int min(int x, int y) { return x < y ? x : y; }
 /*
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
@@ -22,12 +22,68 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
  */
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N]) {
-  int i, j, tmp;
+  if (N == 32) {
+    int tmp, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
+    for (int i = 0; i < N; i += 8) {
+      for (int j = 0; j < M; j++) {
+        tmp = A[i][j];
+        tmp1 = A[i + 1][j];
+        tmp2 = A[i + 2][j];
+        tmp3 = A[i + 3][j];
+        tmp4 = A[i + 4][j];
+        tmp5 = A[i + 5][j];
+        tmp6 = A[i + 6][j];
+        tmp7 = A[i + 7][j];
+        B[j][i] = tmp;
+        B[j][i + 1] = tmp1;
+        B[j][i + 2] = tmp2;
+        B[j][i + 3] = tmp3;
+        B[j][i + 4] = tmp4;
+        B[j][i + 5] = tmp5;
+        B[j][i + 6] = tmp6;
+        B[j][i + 7] = tmp7;
+      }
+    }
+  } else if (N == 67) {
+    // int tmp, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
+    int tmp[8];
+    for (int block_i_start = 0; block_i_start < N; block_i_start += 8) {
+      for (int block_j_start = 0; block_j_start < M; block_j_start += 8) {
+        for (int i = block_i_start; i < min(block_i_start + 8, N); i++) {
+          for (int j = block_j_start; j < min(M, block_j_start + 8); j++) {
+            tmp[j - block_j_start] = A[i][j];
+          }
+          for (int j = block_j_start; j < min(M, block_j_start + 8); j++) {
+            B[j][i] = tmp[j - block_j_start];
+          }
+          //   tmp = A[i][block_j_start];
+          //   tmp1 = A[i][block_j_start + 1];
+          //   tmp2 = A[i][block_j_start + 2];
+          //   tmp3 = A[i][block_j_start + 3];
+          //   tmp4 = A[i][block_j_start + 4];
+          //   tmp5 = A[i][block_j_start + 5];
+          //   tmp6 = A[i][block_j_start + 6];
+          //   tmp7 = A[i][block_j_start + 7];
+          //   B[block_j_start][i] = tmp;
+          //   B[block_j_start + 1][i] = tmp1;
+          //   B[block_j_start + 2][i] = tmp2;
+          //   B[block_j_start + 3][i] = tmp3;
+          //   B[block_j_start + 4][i] = tmp4;
+          //   B[block_j_start + 5][i] = tmp5;
+          //   B[block_j_start + 6][i] = tmp6;
+          //   B[block_j_start + 7][i] = tmp7;
+        }
+      }
+    }
 
-  for (i = 0; i < N; i++) {
-    for (j = 0; j < M; j++) {
-      tmp = A[i][j];
-      B[j][i] = tmp;
+  } else {
+    int i, j, tmp;
+
+    for (i = 0; i < N; i++) {
+      for (j = 0; j < M; j++) {
+        tmp = A[i][j];
+        B[j][i] = tmp;
+      }
     }
   }
 }
@@ -54,11 +110,10 @@ void trans(int M, int N, int A[N][M], int B[M][N]) {
 
 char trans_desc_loop_unroll[] = "loop unroll";
 void trans_loop_unroll(int M, int N, int A[N][M], int B[M][N]) {
-  int i, j;
   // 循环展开。。
   int tmp, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
-  for (i = 0; i < N; i += 8) {
-    for (j = 0; j < M; j++) {
+  for (int i = 0; i < N; i += 8) {
+    for (int j = 0; j < M; j++) {
       tmp = A[i][j];
       tmp1 = A[i + 1][j];
       tmp2 = A[i + 2][j];
@@ -82,40 +137,26 @@ void trans_loop_unroll(int M, int N, int A[N][M], int B[M][N]) {
 char trans_desc_blocking[] = "Simple row-wise scan transpose";
 void trans_blocking(int M, int N, int A[N][M], int B[M][N]) {
   int tmp, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
-  //   for (i = 0; i < N; i++) {
-  //     for (j = 0; j < M; j++) {
-  //       tmp = A[i][j];
-  //       B[j][i] = tmp;
-  //     }
-  //   }
-  for (int block_i = 0; block_i < N / 8; block_i++) {
-    for (int block_j = 0; block_j < M / 8; block_j++) {
-      for (int i = 0; i < 8; i++) {
-        tmp = A[i + 8 * block_i][8 * block_j];
-        tmp1 = A[i + 8 * block_i][8 * block_j + 1];
-        tmp2 = A[i + 8 * block_i][8 * block_j + 2];
-        tmp3 = A[i + 8 * block_i][8 * block_j + 3];
-        tmp4 = A[i + 8 * block_i][8 * block_j + 4];
-        tmp5 = A[i + 8 * block_i][8 * block_j + 5];
-        tmp6 = A[i + 8 * block_i][8 * block_j + 6];
-        tmp7 = A[i + 8 * block_i][8 * block_j + 7];
-
-        B[8 * block_j][i + 8 * block_i] = tmp;
-        B[8 * block_j + 1][i + 8 * block_i] = tmp1;
-        B[8 * block_j + 2][i + 8 * block_i] = tmp2;
-        B[8 * block_j + 3][i + 8 * block_i] = tmp3;
-        B[8 * block_j + 4][i + 8 * block_i] = tmp4;
-        B[8 * block_j + 5][i + 8 * block_i] = tmp5;
-        B[8 * block_j + 6][i + 8 * block_i] = tmp6;
-        B[8 * block_j + 7][i + 8 * block_i] = tmp7;
+  for (int block_i_start = 0; block_i_start < N; block_i_start += 8) {
+    for (int block_j_start = 0; block_j_start < M; block_j_start += 8) {
+      for (int i = block_i_start; i < min(block_i_start + 8, N); i++) {
+        tmp = A[i][block_j_start];
+        tmp1 = A[i][block_j_start + 1];
+        tmp2 = A[i][block_j_start + 2];
+        tmp3 = A[i][block_j_start + 3];
+        tmp4 = A[i][block_j_start + 4];
+        tmp5 = A[i][block_j_start + 5];
+        tmp6 = A[i][block_j_start + 6];
+        tmp7 = A[i][block_j_start + 7];
+        B[block_j_start][i] = tmp;
+        B[block_j_start + 1][i] = tmp1;
+        B[block_j_start + 2][i] = tmp2;
+        B[block_j_start + 3][i] = tmp3;
+        B[block_j_start + 4][i] = tmp4;
+        B[block_j_start + 5][i] = tmp5;
+        B[block_j_start + 6][i] = tmp6;
+        B[block_j_start + 7][i] = tmp7;
       }
-      // for ( int i = 0 ; i < 8 ;  i++){
-      //     for (int j = 0 ; j < 8 ; j++){
-      //         tmp = A[i+8*block_i][j+8*block_j];
-
-      //         B[j+8*block_j][i+8*block_i] = tmp;
-      //     }
-      // }
     }
   }
 }
@@ -131,11 +172,11 @@ void registerFunctions() {
   /* Register your solution function */
   registerTransFunction(transpose_submit, transpose_submit_desc);
   /* Register any additional transpose functions */
-  registerTransFunction(trans, trans_desc);
+  //   registerTransFunction(trans, trans_desc);
 
-  registerTransFunction(trans_loop_unroll, trans_desc_loop_unroll);
+  //   registerTransFunction(trans_loop_unroll, trans_desc_loop_unroll);
 
-//   registerTransFunction(trans_blocking, trans_desc_blocking);
+  registerTransFunction(trans_blocking, trans_desc_blocking);
 }
 
 /*
